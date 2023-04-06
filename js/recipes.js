b5531ee36f5b462f4b4237a86b4b873f1f02005a
Jared Malan <jared.malan7@gmail.com>
2023-04-05 20:39:00 -0600

    api call fixed

--- js/recipes.js
+++ js/recipes.js
@@ -1,28 +1,50 @@
-export function buildRecipes(){
-
+export const buildRecipes = async () => {
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '5165d2660cmsh21763977e15dcabp1914a1jsn1456bd73998f',
-            'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-        }
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '5165d2660cmsh21763977e15dcabp1914a1jsn1456bd73998f',
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+      }
     };
-    
-    fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup', options)
-        .then(response => response.json())
-        .then(response => {
-            const recipesContainer = document.createElement('div');
-            recipesContainer.classList.add('recipes-container');
-
-            response.searches.forEach(search => {
-                const recipeItem = document.createElement('div');
-                recipeItem.classList.add('recipe-item');
-                recipeItem.textContent = search;
-                recipesContainer.appendChild(recipeItem);
-            });
-
-            const main = document.querySelector('main');
-            main.appendChild(recipesContainer);
-        })
-        .catch(err => console.error(err));
-}
+  
+    try {
+      const response = await fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options);
+      const recipes = await response.json();
+  
+      const mainSection = document.querySelector('main');
+      mainSection.innerHTML = '';
+  
+      recipes.results.forEach(recipe => {
+        const recipeEl = document.createElement('div');
+        recipeEl.classList.add('recipe');
+  
+        const recipeLink = document.createElement('a');
+        recipeLink.href = `https://tasty.co/recipe/${recipe.slug}`;
+        recipeLink.target = '_blank';
+  
+        const nameEl = document.createElement('h2');
+        nameEl.textContent = recipe.name;
+        recipeLink.appendChild(nameEl);
+  
+        const thumbnailEl = document.createElement('img');
+        thumbnailEl.src = recipe.thumbnail_url;
+        thumbnailEl.alt = recipe.thumbnail_alt_text || '';
+        recipeLink.appendChild(thumbnailEl);
+  
+        recipeEl.appendChild(recipeLink);
+  
+        const descriptionEl = document.createElement('p');
+        descriptionEl.textContent = recipe.description;
+        recipeEl.appendChild(descriptionEl);
+  
+        const servingsEl = document.createElement('p');
+        servingsEl.textContent = `Servings: ${recipe.yields}`;
+        recipeEl.appendChild(servingsEl);
+  
+        mainSection.appendChild(recipeEl);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
